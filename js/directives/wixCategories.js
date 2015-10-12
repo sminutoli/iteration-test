a350bce39fcc7af53d2db3dce2cdbb00860b3e3f
sminutoli <sminutoli.it@gmail.com>
2015-10-12 12:07:54 -0300

    fixes bug on depths

--- js/directives/wixCategories.js
+++ js/directives/wixCategories.js
@@ -12,7 +12,7 @@
 			scope: {
 				all: '=wixCategories'
 			},
-			link: function(scope, element, attrs){
+			link: function(scope, domElement, attrs){
 
 				//in order to handle the async population we need to watch the collection!
 				var kill = scope.$watchCollection('all', dataChanged);
@@ -23,33 +23,32 @@
 					if(!newVal.length) return;
 
 					var all = newVal;
-					var domElement = element;
 					domElement.empty();
 					
 					for(var i=0; i < all.length; i++){
 
 						var category = all[i];
-						category.next = null;
-
+						
 						var current = category;
 						var last = current;
 
-						var container = domElement;
+						current.next = null;
+						current.container = domElement;
 
 						while (current) {
 						  
 						  var li = angular.element('<li>').text(current.name)
-						  container.append( li );
+						  current.container.append( li );
 
 						  var children = current.subcategories;
 						  
 						  for (var i2 = 0; children && i2 < children.length; i2++) {
 						    var ul = angular.element('<ul>');
 						    li.append(ul);
-						    container = ul;
 
 						    var child = children[i2];
 						    child.next = null;
+						    child.container = ul;
 						    last.next = child;
 						    last = child;
 						    
